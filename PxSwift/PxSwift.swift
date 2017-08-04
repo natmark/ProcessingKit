f070a7da9b86fbf09adfcd908fc2e49042ac7021
natmark <natmark0918@gmail.com>
2017-08-05 00:22:00 +0900

    :wrench: Changing configuration

---
+++ PxSwift/PxSwift.swift
@@ -0,0 +1,13 @@
+//
+//  PxSwift.swift
+//  PxSwift
+//
+//  Created by AtsuyaSato on 2017/08/05.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+import Foundation
+
+open class PxSwift{
+    
+}
