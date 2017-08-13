2ae244676a424a21912f43988227426e0bfd6332
natmark <natmark0918@gmail.com>
2017-08-13 19:46:50 +0900

    :art: Use struct for internal var

--- PxSwift/Event.swift
+++ PxSwift/Event.swift
@@ -8,31 +8,36 @@
 
 import Foundation
 
+struct EventComponents {
+    var fingerTapped = false
+    var fingerMoved = false
+    var fingerReleased = false
+}
+
 extension PxView {
-    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             self.touchX = touch.location(in: self).x
             self.touchY = touch.location(in: self).y
         }
         self.fingerPressed = true
-        self.fingerTapped = true
+        self.eventComponents.fingerTapped = true
     }
-    open override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
+
+    open override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             self.touchX = touch.location(in: self).x
             self.touchY = touch.location(in: self).y
         }
-        self.fingerMoved = true
+        self.eventComponents.fingerMoved = true
     }
-    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
+
+    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             self.touchX = touch.location(in: self).x
             self.touchY = touch.location(in: self).y
         }
         self.fingerPressed = false
-        self.fingerReleased = true
+        self.eventComponents.fingerReleased = true
     }
 }
