509537fbb598f34f1a3665063a349caab002e0d0
natmark <natmark0918@gmail.com>
2017-09-27 13:50:07 +0900

    :new: Add vertex model protocol

---
+++ ProcessingKit/Shape/Vertex.swift
@@ -0,0 +1,15 @@
+//
+//  Vertex.swift
+//  ProcessingKit
+//
+//  Created by AtsuyaSato on 2017/09/27.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+import Foundation
+
+protocol VerTexModelContract {
+    func beginShape()
+    func endShape()
+    func vertex()
+}
