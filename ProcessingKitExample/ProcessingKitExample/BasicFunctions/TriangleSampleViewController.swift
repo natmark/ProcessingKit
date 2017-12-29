dec9c9ec79c56aace8ea0c4b3713c68a196c811a
natmark <natmark0918@gmail.com>
2017-12-29 23:29:38 +0900

    :sparkles: Add Triangle function

---
+++ ProcessingKitExample/ProcessingKitExample/BasicFunctions/TriangleSampleViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TriangleSampleViewController.swift
+//  ProcessingKitExample
+//
+//  Created by AtsuyaSato on 2017/12/29.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+import UIKit
+
+class TriangleSampleViewController: UIViewController {
+
+    @IBOutlet weak var triangleSampleView: TriangleSampleView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
