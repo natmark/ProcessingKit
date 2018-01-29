51a60e5a511a26620f3beb0146aa82ed91b383cd
natmark <natmark0918@gmail.com>
2018-01-29 22:31:23 +0900

    :art: fix shapeTest structure

--- ProcessingKitTests/ShapeTests.swift
+++ ProcessingKitTests/ShapeTests.swift
@@ -9,32 +9,48 @@
 import XCTest
 @testable import ProcessingKit
 
-class ShapeDelegateSpy: ProcessingViewDelegate {
+enum Shape {
+    case rect(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat)
+}
+
+class ProcessingViewDelegateShapeSpy: ProcessingViewDelegate {
     private let exception: XCTestExpectation
     private let view: ProcessingView
-    private let x: CGFloat
-    private let y: CGFloat
-    private let width: CGFloat
-    private let height: CGFloat
+    private let shape: Shape
     private(set) var context: CGContext?
 
-    init(exception: XCTestExpectation, view: ProcessingView, x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) {
+    init(exception: XCTestExpectation, view: ProcessingView, shape: Shape) {
         self.exception = exception
         self.view = view
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+        self.shape = shape
     }
 
     func setup() {
-        self.view.rect(x, y, width, height)
+        switch shape {
+        case .rect(let x, let y, let width, let height):
+            self.view.rect(x, y, width, height)
+        }
         self.record(UIGraphicsGetCurrentContext())
         exception.fulfill()
     }
 
     private func record(_ arg: CGContext?) {
         self.context = arg
+    }
+}
+
+extension UIBezierPath {
+    open func moveTo(_ point: CGPoint) -> UIBezierPath {
+        self.move(to: point)
+        return self
+    }
+    open func addLineTo(_ point: CGPoint) -> UIBezierPath {
+        self.addLine(to: point)
+        return self
+    }
+    open func closePath() -> UIBezierPath {
+        self.close()
+        return self
     }
 }
 
@@ -48,28 +64,57 @@ class ShapeTests: XCTestCase {
     }
 
     func testRect() {
-        let view = ProcessingView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        let testCases: [UInt: TestCase] = [
+            #line: TestCase(
+                description: "draw rect(0, 0, 50, 50)",
+                shape: .rect(x: 0, y: 0, width: 50, height: 50),
+                expect: UIBezierPath()
+                    .moveTo(CGPoint(x: 0, y: 0))
+                    .addLineTo(CGPoint(x: 50, y: 0))
+                    .addLineTo(CGPoint(x: 50, y: 50))
+                    .addLineTo(CGPoint(x: 0, y: 50))
+                    .closePath()
+                    .cgPath
+            ),
+            #line: TestCase(
+                description: "draw rect(20, 20, 30, 50)",
+                shape: .rect(x: 20, y: 20, width: 30, height: 50),
+                expect: UIBezierPath()
+                    .moveTo(CGPoint(x: 20, y: 20))
+                    .addLineTo(CGPoint(x: 50, y: 20))
+                    .addLineTo(CGPoint(x: 50, y: 70))
+                    .addLineTo(CGPoint(x: 20, y: 70))
+                    .closePath()
+                    .cgPath
+            ),
+        ]
 
-        let shapeDelegateSpy = ShapeDelegateSpy(
-            exception: expectation(description: "Shape"),
-            view: view,
-            x: 0,
-            y: 0,
-            width: 10,
-            height: 10
-        )
+        check(testCases: testCases)
+    }
 
-        view.delegate = shapeDelegateSpy
-        waitForExpectations(timeout: 100)
+    func check(testCases: [UInt: TestCase]) {
+        _ = testCases.map { (line, testCase) in
+            let view = ProcessingView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 
-        let actual = shapeDelegateSpy.context?.path
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: 0, y: 0))
-        path.addLine(to: CGPoint(x: 10, y: 0))
-        path.addLine(to: CGPoint(x: 10, y: 10))
-        path.addLine(to: CGPoint(x: 0, y: 10))
-        path.close()
-        let expected = path.cgPath
-        XCTAssertEqual(actual, expected, String(#line))
+            let transformDelegateSpy = ProcessingViewDelegateShapeSpy(
+                exception: expectation(description: testCase.description),
+                view: view,
+                shape: testCase.shape
+            )
+
+            view.delegate = transformDelegateSpy
+            waitForExpectations(timeout: 100)
+
+            let actual = transformDelegateSpy.context?.path
+            let expected = testCase.expect
+
+            XCTAssertEqual(actual, expected, String(line))
+        }
+    }
+
+    struct TestCase {
+        let description: String
+        let shape: Shape
+        let expect: CGPath
     }
 }
