a2618f132829a3a7765bfa5e88243bb8953736b6
natmark <natmark0918@gmail.com>
2018-10-27 19:43:07 +0900

    Fix ProcessingView+Gesture

--- ProcessingKit/ProcessingView+Core/ProcessingView+Gesture.swift
+++ ProcessingKit/ProcessingView+Core/ProcessingView+Gesture.swift
@@ -14,7 +14,7 @@ import UIKit
 import Cocoa
 #endif
 
-extension ProcessingView: GestureModelContract {
+extension ProcessingView {
     #if os(iOS)
     public var fingerPressed: Bool {
         return self.gestureModel.fingerPressed
@@ -46,19 +46,7 @@ extension ProcessingView: GestureModelContract {
     #endif
 
     #if os(iOS)
-    public func touchesBegan(_ touches: Set<CGPoint>) {
-        self.gestureModel.touchesBegan(touches)
-    }
-
-    public func touchesMoved(_ touches: Set<CGPoint>) {
-        self.gestureModel.touchesMoved(touches)
-    }
-
-    public func touchesEnded(_ touches: Set<CGPoint>) {
-        self.gestureModel.touchesEnded(touches)
-    }
-
-    @objc public func didTap(recognizer: UITapGestureRecognizer) {
+    @objc func didTap(recognizer: UITapGestureRecognizer) {
         self.gestureModel.didTap(recognizer: recognizer)
 
         let dispatchTime = DispatchTime.now() + Double(1.0 / self.frameRate)
@@ -66,38 +54,26 @@ extension ProcessingView: GestureModelContract {
             self.didTapExit(recognizer: recognizer)
         }
     }
-    public func didTapExit(recognizer: UITapGestureRecognizer) {
+    func didTapExit(recognizer: UITapGestureRecognizer) {
         self.gestureModel.didTapExit(recognizer: recognizer)
     }
-    @objc public func didSwipe(recognizer: UISwipeGestureRecognizer) {
+    @objc func didSwipe(recognizer: UISwipeGestureRecognizer) {
         self.gestureModel.didSwipe(recognizer: recognizer)
     }
-    @objc public func didPinch(recognizer: UIPinchGestureRecognizer) {
+    @objc func didPinch(recognizer: UIPinchGestureRecognizer) {
         self.gestureModel.didPinch(recognizer: recognizer)
     }
-    @objc public func didRotate(recognizer: UIRotationGestureRecognizer) {
+    @objc func didRotate(recognizer: UIRotationGestureRecognizer) {
         self.gestureModel.didRotate(recognizer: recognizer)
     }
-    @objc public func didPan(recognizer: UIPanGestureRecognizer) {
+    @objc func didPan(recognizer: UIPanGestureRecognizer) {
         self.gestureModel.didPan(recognizer: recognizer)
     }
-    @objc public func didLongPress(recognizer: UILongPressGestureRecognizer) {
+    @objc func didLongPress(recognizer: UILongPressGestureRecognizer) {
         self.gestureModel.didLongPress(recognizer: recognizer)
     }
     #elseif os(OSX)
-    public func mouseDown(_ location: NSPoint) {
-        self.gestureModel.mouseDown(location)
-    }
-    public func mouseDragged(_ location: NSPoint) {
-        self.gestureModel.mouseDragged(location)
-    }
-    public func mouseUp(_ location: NSPoint) {
-        self.gestureModel.mouseUp(location)
-    }
-    public func mouseMoved(_ location: NSPoint) {
-        self.gestureModel.mouseMoved(location)
-    }
-    @objc public func didClick(recognizer: NSClickGestureRecognizer) {
+    @objc func didClick(recognizer: NSClickGestureRecognizer) {
         self.gestureModel.didClick(recognizer: recognizer)
 
         let dispatchTime = DispatchTime.now() + Double(1.0 / self.frameRate)
@@ -105,25 +81,25 @@ extension ProcessingView: GestureModelContract {
             self.didClickExit(recognizer: recognizer)
         }
     }
-    @objc public func didClickExit(recognizer: NSClickGestureRecognizer) {
+    @objc func didClickExit(recognizer: NSClickGestureRecognizer) {
         self.gestureModel.didClickExit(recognizer: recognizer)
     }
-    @objc public func didMagnify(recognizer: NSMagnificationGestureRecognizer) {
+    @objc func didMagnify(recognizer: NSMagnificationGestureRecognizer) {
         self.gestureModel.didMagnify(recognizer: recognizer)
     }
-    @objc public func didPan(recognizer: NSPanGestureRecognizer) {
+    @objc func didPan(recognizer: NSPanGestureRecognizer) {
         self.gestureModel.didPan(recognizer: recognizer)
     }
-    @objc public func didPress(recognizer: NSPressGestureRecognizer) {
+    @objc func didPress(recognizer: NSPressGestureRecognizer) {
         self.gestureModel.didPress(recognizer: recognizer)
     }
-    @objc public func didRotate(recognizer: NSRotationGestureRecognizer) {
+    @objc func didRotate(recognizer: NSRotationGestureRecognizer) {
         self.gestureModel.didRotate(recognizer: recognizer)
     }
-    open override func scrollWheel(with event: NSEvent) {
+    open func scrollWheel(with event: NSEvent) {
         self.gestureModel.scrollWheel(with: event)
     }
-    open override func mouseMoved(with event: NSEvent) {
+    open func mouseMoved(with event: NSEvent) {
         self.gestureModel.mouseMoved(with: event)
     }
     #endif
